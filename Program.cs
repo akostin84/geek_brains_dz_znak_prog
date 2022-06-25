52662d36a15a3c48847e6fc7ca5ce1c86628ebf3
andrei_kostin <akostin84@yandex.com>
2022-06-25 09:03:01 +0300

    previous tasks removed

--- Program.cs
+++ Program.cs
@@ -1,107 +1,8 @@
-﻿using System;
-/* 
-Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-14212 -> нет
-23432 -> да
-12821 -> да 
-*/
-
-string AskNumber()
+﻿string AskNumber()
 {
     Console.Write("Введите ваше число: ");
     string number = Console.ReadLine();
     return number;
-}
-
-
-int ReverseNumber(int n)
-{
-    int temp = n;
-    int reversed = 0;
-    while (n > 0)
-    {
-        // остаток от деления на 10
-        int digit = n % 10;
-        reversed = reversed * 10 + digit;
-        // целочисленное деление на 10
-        n = n / 10;
-    }
-    return reversed;
-}
-
-void task19()
-{
-    Console.WriteLine("*Задача 19. Проверка на палиндром*");
-    int n = Convert.ToInt32(AskNumber());
-    int a = ReverseNumber(n);
-    if (n == a)
-    {
-        Console.WriteLine("да");
-    }
-    else
-    {
-        Console.WriteLine("нет");
-    }
-}
-
-/*
-Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53
-*/
-
-
-int[] AskCoordinate()
-{
-    int[] coordinates = new int[3];
-    int ctr = 0;
-    int temp = 0;
-    while (ctr < 3)
-    {
-        Console.WriteLine("введите координату");
-        coordinates[ctr] = Convert.ToInt32(Console.ReadLine());
-        ctr++;
-    }
-    return coordinates;
-}
-
-double GetDistance(int[] a, int[] b)
-{
-    double distance = Math.Sqrt(Math.Pow((a[0] - b[0]), 2) + Math.Pow((a[1] - b[1]), 2) + Math.Pow((a[2] - b[2]), 2));
-    return distance;
-}
-
-
-void task21()
-{
-    Console.WriteLine("*Задача 21. Расстояние между 2 точками по 3 координатам*");
-    Console.WriteLine("Первая точка");
-    int [] PointA = AskCoordinate();
-    Console.WriteLine("Вторая точка");
-    int [] PointB = AskCoordinate();
-    Console.WriteLine(GetDistance(PointA, PointB));
-}
-
-
-/*
-Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27.
-5 -> 1, 8, 27, 64, 125
-*/
-
-void PrintPower3(int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        Console.WriteLine(Math.Pow(i, 3));
-    }
-}
-
-void task23()
-{
-    Console.WriteLine("*Задача 23. Вывод кубов*");
-    int n = Convert.ToInt32(AskNumber());
-    PrintPower3(n);
 }
 
 /*
@@ -216,11 +117,6 @@ void task29()
     PrintArray(b);
 }
 
-//task19();
-//task21();
-//task23();
-
 task25();
 task27();
 task29();
-
