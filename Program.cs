c8a1083661f5b6a0fbce004bb0a471955a730826
andrei_kostin <akostin84@yandex.com>
2022-07-02 17:45:43 +0300

    task 47 added

--- Program.cs
+++ Program.cs
@@ -57,5 +57,51 @@ void task43()
     Console.WriteLine($"Точка пересечения: ({xCross}, {yCross})");
 }
 
-task41();
-task43();
+/*
+Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[,] FillArray(int rows, int columns)
+{
+    double[,] randomArray = new double[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows ; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            randomArray[i, j] = rand.NextDouble() * 10 - 5;
+        }
+    }
+    return randomArray;
+}
+
+void PrintMatrix(double[,] matrixToPrint)
+{
+    Console.WriteLine("Вывод двумерного массива на печать...");
+    for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+        {
+            Console.Write(matrixToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void task47()
+{
+    Console.WriteLine("*Задача 47. Задание двумерного массива действительных чисел");
+    Console.Write("Введите число строк массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.Write("Введите число столбцов массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    double[,] randMatrix = FillArray(n, m);
+    PrintMatrix(randMatrix);
+}
+
+task47();
